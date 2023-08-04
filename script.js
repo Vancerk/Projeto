5a8d774d8d4ccb1e7abef7a343ba41a3bb54626f
Vancerk <vanxf@outlook.pt>
2023-08-03 23:32:21 -0300

    ultimas

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ function togglemode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./Assets/Avatarlight.png")
+    img.setAttribute("src", "./Assets/avatarlight.png")
     img.setAttribute("alt", "foto de um cara assustado")
   } else {
     img.setAttribute("src", "./Assets/avatar1.png")
-    img.setAttribute(
-      "alt",
-      "foto de pessoa comum fumando um chongos fundo bege camisa preta"
-    )
+    img.setAttribute("alt", "foto de pessoa comum fumando um chongos fundo bege camisa preta")
   }
 }
